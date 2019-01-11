c84906f18ab17e357178171904830b540c8c59ca
Félix Baylac-Jacqué <felix@alternativebit.fr>
2019-01-11 16:37:58 +0100

    Migrate rules_haskell to Bazel 0.21.
    Fixed toolchain implicit types
    ===
    Toolchain-type labels should now point to an actual rule.
    
    Docs build tools cleanup
    ===
    The host path is not forwarded anymore to the sandbox in charge of
    executing the actions. This means we now need to create some specific
    rules for each tool using during the build process of the documentation.
    
    Protolens configuration update
    ===
    - Updating the protolens flags.
    - Protolens is now using deepseq in its generated files, we need to add
      this library as a toolchain dependency.

--- nixpkgs/default.nix
+++ nixpkgs/default.nix
@@ -1,5 +1,5 @@
 import (fetchTarball {
-   # Nixpkgs checkout from 2018-11-03.
-   url = "https://github.com/NixOS/nixpkgs/archive/cb692e6f52f6d4ca619c9fbc7a80afc081a93103.tar.gz";
-   sha256 = "1aww7wjgv868asd4yl0wnzh8qa26j4wsmqn85b3s9fkqkz4p2m0f";
+   # Nixpkgs checkout from 2019-01-15.
+   url = "https://github.com/NixOS/nixpkgs/archive/da8e0491bc9bd9ee193e71776dc7279b2e5efef3.tar.gz";
+   sha256 = "1g6fqr5f5126jw7yf9wxg5wg03dz59kypm7x6g4w67w090dda30l";
 })
